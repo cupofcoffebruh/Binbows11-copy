eea92a47c1656eb08e7150d001d01045ee60ecb0
Cup Of Coffe <90518786+cupofcoffebruh@users.noreply.github.com>
2021-10-27 18:20:42 +0200

    Update camera.js

--- apps/camera/camera.js
+++ apps/camera/camera.js
@@ -1,1 +1,15 @@
+let canvas = document.querySelector("#canvas")
+let video = document.querySelector("#video");
+let context = canvas.getContext("2d");
 
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) =>{
+    video.srcObject = stream;
+    video.play();
+    });
+}
+
+document.getElementById("snap").addEventListener("click", () =>{
+  context.drawImage(video, 0, 0, 45, 45);
+
+});
